36e637c54be4383ddacf6be171450d1a79d66bd0
PabloMartinTomasi <pablo.martintomasi24@campusfp.es>
2024-10-31 12:14:21 +0100

    unos cambios

--- Ejercicios/Ejercicio 13/EJ.13.4.py
+++ Ejercicios/Ejercicio 13/EJ.13.4.py
@@ -5,7 +5,8 @@ numeros = [random.randint (-50, 50) for i in range(15)]
 with open("valores.txt", "w") as archivo:
     archivo.write(f"{numeros}\n")
 with open("valores.txt", "r") as archivo:
-    contenido = archivo.read()
+    lineas = archivo.read()
 
-positivos = np.array[contenido > 0]
+array = np.array([int(linea.strip()) for linea in lineas])
+positivos = np.array[array > 0]
 print(positivos)
