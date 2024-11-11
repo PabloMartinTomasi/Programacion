5d728773557144d7498e2b8d51b296652b14782b
PabloMartinTomasi <pablo.martintomasi24@campusfp.es>
2024-11-11 12:13:43 +0100

    hito programacion

--- hito 2/Realizar_compra.py
+++ hito 2/Realizar_compra.py
@@ -16,3 +16,18 @@ while True:
         idproducto, nombre, medida, precio, stock = producto
         print(Fore.BLUE+ f"{idproducto} - {nombre} - {medida} - {precio} - {stock}")
         print(Style.RESET_ALL)
+        
+    idcliente = int(input("Introduce tu idcliente:"))
+    idproducto = int(input("Introduce el idproducto que quieres agregar:"))
+    unidades = int(input("Introduce las unidades que quieres agregar:"))
+    añadir_producto_a_comprar = (idcliente, idproducto, unidades)
+    consulta = """INSERT INTO detalle (idcliente, idproducto, unidades) VALUES (%s, %s, %s)"""
+    cursor.execute(consulta, añadir_producto_a_comprar)
+    conexion.commit()
+    print(Fore.GREEN+ f"[Mensaje de confirmacion] se a añadidio {idproducto} ha sido creada con éxito.")
+    print(Style.RESET_ALL)
+    if cursor:
+        cursor.close()
+    if conexion:
+        conexion.close()
+    break
