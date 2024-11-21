b7e87a20adbef360d8c21076cc74a7e0dc9807de
PabloMartinTomasi <pablo.martintomasi24@campusfp.es>
2024-11-21 14:43:03 +0100

    ejercicio 19

--- Ejercicio 19/ejercicio_19.py
+++ Ejercicio 19/ejercicio_19.py
@@ -12,28 +12,27 @@ while True:
         print(Fore.RED+ "1- Administrador \n2- Usuario regular \n3- Salir")
         print(Style.RESET_ALL)
         menu = int(input("Seleciona una opcion:"))
-        while True:
-            if menu == 1: #Si es un administrados
-                print(Fore.RED+ "=== Gestión del Centro Deportivo === \n1- Gestión de Clientes \n2- Gestión de Actividades \n3- Gestión de Entrenadores \n4- Gestión de Inscripciones \n5- Filtros y busqueda \n- Salir") #Enseñamos el menu de la gestion del centro de deporte
+        if menu == 1: #Si es un administrados
+            print(Fore.RED+ "=== Gestión del Centro Deportivo === \n1- Gestión de Clientes \n2- Gestión de Actividades \n3- Gestión de Entrenadores \n4- Gestión de Inscripciones \n5- Filtros y busqueda \n- Salir") #Enseñamos el menu de la gestion del centro de deporte
+            print(Style.RESET_ALL)
+            Menu = int(input("Seleccione una opción:")) #Tenemos que selecionar una opcion del menu
+            if Menu == 1:
+                CLI.menu_cliente() #Si la opcion 1 es selecionada veremos el menu cliente
+            elif Menu == 2:
+                ACT.menu_actividades() #Si la opcion 2 es selecionada veremos el menu actividades
+            elif Menu == 3:
+                ENT.menu_entrenadores() #Si la opcion 3 es selecionada veremos el menu de entrenadores
+            elif Menu == 4:
+                INS.menu_inscripciones() #Si la opcion 4 es selecionada veremos el menu de inscripciones
+            elif Menu == 5:
+                print("[Mensaje] Saliendo de la gestión. ¡Hasta pronto!") #Si selecionamos la opcion 5 salimos del programa
+                break
+            else:
+                print(Fore.YELLOW+ "Seleciona una opcion del menu") #Si selecionamos una opcion que no esta en el menu, se nos vuelve a solicitar
                 print(Style.RESET_ALL)
-                Menu = int(input("Seleccione una opción:")) #Tenemos que selecionar una opcion del menu
-                if Menu == 1:
-                    CLI.menu_cliente() #Si la opcion 1 es selecionada veremos el menu cliente
-                elif Menu == 2:
-                    ACT.menu_actividades() #Si la opcion 2 es selecionada veremos el menu actividades
-                elif Menu == 3:
-                    ENT.menu_entrenadores() #Si la opcion 3 es selecionada veremos el menu de entrenadores
-                elif Menu == 4:
-                    INS.menu_inscripciones() #Si la opcion 4 es selecionada veremos el menu de inscripciones
-                elif Menu == 5:
-                    print("[Mensaje] Saliendo de la gestión. ¡Hasta pronto!") #Si selecionamos la opcion 5 salimos del programa
-                    break
-                else:
-                    print(Fore.YELLOW+ "Seleciona una opcion del menu") #Si selecionamos una opcion que no esta en el menu, se nos vuelve a solicitar
-                    print(Style.RESET_ALL)
-                    continue
+                continue
                 
-            elif menu == 2: #Si es un usuario regular
+        elif menu == 2: #Si es un usuario regular
                 print("===Gestion de inscripciones=== \n1- Quieres inscribirte \n2- Salir")
                 Menu = int(input("Seleciona una opcion del menu:"))
                 if Menu == 1: #Poder hacer inscripciones
@@ -45,13 +44,12 @@ while True:
                     print(Fore.YELLOW+ "Seleciona una opcion del menu")
                     print(Style.RESET_ALL)
                     continue
-            elif menu == 3: #Salir
+        elif menu == 3: #Salir
                 print("[Mensaje] Saliendo. ¡Hasta pronto!")
                 break
-            else:
+        else:
                 print(Fore.YELLOW+ "Seleciona una opcion del menu")
                 print(Style.RESET_ALL)
-                continue
 
     except ValueError as ve:
         print(Fore.RED + f"Error: Entrada inválida. Por favor, ingrese un número. ({ve})")
--- Ejercicio 19/funcion_Filtros_Busqueda.py
+++ Ejercicio 19/funcion_Filtros_Busqueda.py
@@ -14,17 +14,34 @@ def menu_filtro_busqueda():
             print(Style.RESET_ALL)
             if menu == 1: #Filtrar
                 while True:
-                    print(Fore.RED+ "===Filtrar=== \n1- Clientes tipo membresia \n2- Rango de edad \n3- Salir")
+                    print(Fore.RED+ "===Filtrar=== \n1- Membresi \n2- Edad \n3- Salir")
                     print(Style.RESET_ALL)
                     filtrar = int(input("Seleciona una opcion del menu:"))
                     if filtrar == 1:
-                        
-                        
-                        
+                        tipo_membresia()
+                    elif filtrar == 2:
+                        rango_edad()
+                    elif filtrar == 3:
+                        break
+                    else:
+                        print("Seleciona una opcion del menu")
+                       
             elif menu == 2: #Busqueda
                 while True:
-                    print(Fore.RED+ "===Filtros y busqueda=== \n1- Filtrar \n2- Busqueda \n3- Salir")
+                    print(Fore.RED+ "===Busqueda=== \n1- actividades por horario \n2- duración  \n3- especialidad del entrenador \n4- Salir")
                     print(Style.RESET_ALL)
+                    Filtrar = int(input("Seleciona una opcion del menu:"))
+                    if Filtrar == 1:
+                        buscar_actividades()
+                    elif Filtrar == 2:
+                        buscar_duracion()
+                    elif Filtrar == 3:
+                        especialidad_entrenador()
+                    elif Filtrar == 4:
+                        break
+                    else:
+                        print("Seleciona una opcion del menu")
+    
             elif menu == 3: #Salir
                 break
             else:
@@ -49,16 +66,67 @@ def menu_filtro_busqueda():
         
         
 def tipo_membresia():
-    print("Tipo de membresia \n1- Mensual \n2- Trimestral \n3- Anual \n4- Salir")
-    membresia = int(input("Seleciona una opcion del menu:"))
-    while True:
-        if membresia == 1:
-            consulta = """SELECT id_cliente, nombre, edad, tipo_membresia FROM clientes
-                    WHERE tipo_membresia = Anual
-                    order by tipo_membresia;"""
-            cursor.execute(consulta)
-            clientes = cursor.fetchall()
-            print(Fore.YELLOW+"Listado de clientes: \nid_cliente | nombre | edad | tipo_membresia")
-            print(Style.RESET_ALL)
-            for id_cliente, nombre, edad, tipo_membresia in clientes:
-                print(f"[Mensaje]: {id_cliente} | {nombre} | {edad} | {tipo_membresia}")
+    print("Tipo de membresia")
+    tipo_membresia = input("Que tipo de membresi quieres ver(Mensual, Trimestral, Anual):")
+    consulta = """SELECT id_cliente, nombre, edad, tipo_membresia FROM clientes
+                WHERE tipo_membresia = %s
+                order by tipo_membresia"""
+    cursor.execute(consulta, (tipo_membresia,))
+    membresia = cursor.fetchall()
+    print(f"Listado de clientes con la membresi de {tipo_membresia} \nid_cliente | Nombre | Edad | tipo_membresia")
+    for id_cliente, nombre, edad, tipo_membresia in membresia:
+         print(f"[Mensaje]: {id_cliente} | {nombre} | {edad} | {tipo_membresia}")
+         
+def rango_edad():
+    print("Rango de edad")
+    edad = int(input("Busca los clientes con una edad especifica:"))
+    consulta = """SELECT id_cliente, nombre, edad, tipo_membresia FROM clientes
+                WHERE edad = %s
+                order by edad"""
+    cursor.execute(consulta, (edad,))
+    edades = cursor.fetchall()
+    print(f"Listado de clientes con {edad} año \nid_cliente | Nombre | Edad | tipo_membresia")
+    for id_cliente, nombre, edad, tipo_membresia in edades:
+        print(f"[Mensaje]: {id_cliente} | {nombre} | {edad} | {tipo_membresia}")
+         
+         
+         
+         
+def buscar_actividades():
+    print("Actividades por horario")
+    horario = input("Pon el horario de la actividad que quieres buscar:")
+    consulta = """SELECT id_actividad, nombre_actividad, horario, duracion, id_entrenador FROM actividades
+                WHERE horario = %s
+                order by horario"""
+    cursor.execute(consulta,(horario,))
+    actividades = cursor.fetchall()
+    print(f"Listado de actividades con el horario de {horario} \nid_actividad | Nombre de la actividad | Horario | Duracion | id_entrenador")
+    for id_actividad, nombre_actividad, horario, duracion, id_entrenador in actividades:
+        print(f"[Mensaje]: {id_actividad} | {nombre_actividad} | {horario} | {duracion} | {id_entrenador}")
+        
+def buscar_duracion():
+    print("Actividades por Duracion")
+    duracion = int(input("Pon el tiempo de la duracion de la actividad:"))
+    consulta = """SELECT id_actividad, nombre_actividad, horario, duracion, id_entrenador FROM actividades
+                WHERE duracion = %s
+                order by duracion"""
+    cursor.execute(consulta,(duracion,))
+    tiempo = cursor.fetchall()
+    print(f"Listado de actividades con la duracion de {duracion} minutos \nid_actividad | Nombre de la actividad | Horario | Duracion | id_entrenador")
+    for id_actividad, nombre_actividad, horario, duracion, id_entrenador in tiempo:
+        print(f"[Mensaje]: {id_actividad} | {nombre_actividad} | {horario} | {duracion} | {id_entrenador}")
+        
+def especialidad_entrenador():
+    print("Especialidad del entrenador")
+    especialidad = input("Pon la especialidad del entrenador:")
+    consulta = """SELECT A.id_actividad, A.nombre_actividad, A.horario, A.duracion, E.id_entrenador, E.nombre_entrenador, E.especialidad 
+                FROM actividades A
+                INNER JOIN entrenadores E ON 
+                A.id_entrenador = E.id_entrenador
+                WHERE E.especialidad = %s
+                order by E.especialidad"""
+    cursor.execute(consulta,(especialidad,))
+    especialidades = cursor.fetchall()
+    print(f"Listado de actividades de la especialidad de {especialidad} \nid_actividad | Nombre de la actividad | Horario | Duracion | id_entrenador | Nombre entrenador | especialidad")
+    for id_actividad, nombre_actividad, horario, duracion, id_entrenador, nombre_entrenador, especialidad in especialidades:
+        print(f"[Mensaje]: {id_actividad} | {nombre_actividad} | {horario} | {duracion} | {id_entrenador} | {nombre_entrenador} | {especialidad}")
