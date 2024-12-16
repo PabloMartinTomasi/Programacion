41e1a6f86027b14d7121749ee5aa72d058ae0677
PabloMartinTomasi <pablo.martintomasi24@campusfp.es>
2024-12-16 12:16:52 +0100

    php

---
+++ proyecto4/modelo/tablas.php
@@ -0,0 +1,27 @@
+<?php
+function generarTabla($archivoCSV, $tipo) {
+    $tabla = "<a href='vistas/formulario_" . $tipo . ".html' class='btn btn-primary mb-3'>Añadir Nuevo " . ucfirst($tipo) . "</a>
+    <a href='vistas/formulario_" . $tipo . ".html' class='btn btn-primary mb-3'>Eliminar " . ucfirst($tipo) . "</a>
+    <a href='vistas/formulario_" . $tipo . ".html' class='btn btn-primary mb-3'>Modificar " . ucfirst($tipo) . "</a>";
+    $tabla .= "<table class='table table-striped'>";
+
+    if (($archivo = fopen($archivoCSV, "r")) !== false) {
+        $cabeceras = fgetcsv($archivo);
+        $tabla .= "<thead><tr>";
+        foreach ($cabeceras as $cabecera) {
+            $tabla .= "<th>" . htmlspecialchars($cabecera) . "</th>";
+        }
+        $tabla .= "</tr></thead><tbody>";
+        while (($fila = fgetcsv($archivo)) !== false) {
+            $tabla .= "<tr>";
+            foreach ($fila as $celda) {
+                $tabla .= "<td>" . htmlspecialchars($celda) . "</td>";
+            }
+            $tabla .= "</tr>";
+        }
+        fclose($archivo);
+    }
+    $tabla .= "</tbody></table>";
+    return $tabla;
+}
+?>
